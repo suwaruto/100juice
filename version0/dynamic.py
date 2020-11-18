1c3e87592ffb5e5ce4e91e3ce61254d3b81fb7b2
Ovinnikov Nikita <gondork12@gmail.com>
2020-11-19 02:17:50 +0300

    On branch master  Changes to be committed: 	deleted:    __pycache__/dynamic.cpython-38.pyc 	deleted:    __pycache__/events.cpython-38.pyc 	deleted:    __pycache__/game_master.cpython-38.pyc 	deleted:    __pycache__/tiledmap.cpython-38.pyc 	deleted:    ai.py 	modified:   dynamic.py 	deleted:    events.py 	modified:   game_master.py 	modified:   main.py 	modified:   tiledmap.py
     Added an attack option for players.

--- version0/dynamic.py
+++ version0/dynamic.py
@@ -1,3 +1,8 @@
+def str_list(lst, starting_index = 0):
+    res = ""
+    for i in range(len(lst)):
+        res += str(i + starting_index) + ": " + str(lst[i]) 
+        
 class Dynamic(object):
     
     def __init__(self, x, y):
@@ -12,21 +17,22 @@ class Creature(Dynamic):
         self._inventory = inventory
         self._velocity = (1, 4) # tuple[0] d tuple[1]
         self._ac = 10
-        self._attack = 0
+        self._attack = (1, 4)
 
     def OnMove(self, x, y, ind):
         self._x = x
         self._y = y
         self._ind = ind
 
-    def OnAttack(self, dmg):
-       hp -= dmg 
-
     def OnKill(self, creature):
         self._inventory += creature.GetInventory()
 
     def OnDeath(self):
         self.__del__()
+
+    def OnAttack(self):
+        if self._hp <= 0:
+            self.OnDeath()     
 
     def GetInventory(self):
         return self._inventory
@@ -44,4 +50,7 @@ class Player(Creature):
        self._inventory = []
 
     def QueryAction(self):
-        return int(input(self._name + ": What action would you like to perform?"))
+        return int(input(self._name + ": What action would you like to perform? "))
+        
+    def IsActive(self):
+        return self._active
